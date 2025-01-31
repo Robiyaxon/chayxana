db567bcdc56c3c26b47f295c4dd5c84f17bb9df4
Robiyaxon <tuhtaevarobiahon@gmail.com>
2025-01-31 18:56:05 +0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-// import Home from './component/home/Home';
+import Home from './component/home/Home';
 import FavooriteFood from './component/favorite/FavooriteFood';
-function App() {
+
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import img from "./assert/logo.png"
+const Preloader = () => {
+  return (
+    <div className="preloader fade-out">
+      <img src={img} alt="Logo" className="logo" />
+    </div>
+  );
+};
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3500);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <BrowserRouter>
+      {loading ? <Preloader /> : <>  <div className='app'>
+        <Home />
+        <FavooriteFood /> </div>
+      </>}
 
-   <div className='app'>
-    {/* <Home/> */}
-    <FavooriteFood/>
-   </div>
-   </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
